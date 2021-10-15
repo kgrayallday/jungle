968ba0072efd92fd4bacaafac1a19f8c3b477268
Kyle Gray <kylemaxgray@gmail.com>
2021-10-15 07:26:03 +0000

    implemented admin dashboard with counts - buttons have placeholder values for now

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,10 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["HTTP_BASIC_AUTH_USER"], password: ENV["HTTP_BASIC_AUTH_PASS"]
+  
 
   def show
+    @products = Product.all.length
+    @categories = Category.all.length
+
   end
 end
